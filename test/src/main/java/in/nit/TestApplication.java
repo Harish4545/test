1ed1f70e67f8fef2a93f5e1c10d448b9701267ed
Harish4545 <harishchowdary82@gmail.com>
2023-02-01 12:10:26 +0530

    hhhhhhhh

---
+++ test/src/main/java/in/nit/TestApplication.java
@@ -0,0 +1,22 @@
+package in.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+
+public class TestApplication {
+	
+	@GetMapping("/")
+	public String getting() {
+		return "hi";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
